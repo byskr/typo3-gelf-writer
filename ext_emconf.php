3375f536f7698937b064154b33787ce3a6ee923c
Daniel Weise <daniel.weise@tourstream.eu>
2016-11-25 11:17:54 +0100

    Initial

---
+++ ext_emconf.php
@@ -0,0 +1,29 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array (
+    'title' => 'FTI Graylog Writer',
+    'description' => '',
+    'category' => 'plugin',
+    'version' => '0.0.1',
+    'state' => 'beta',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearcacheonload' => 0,
+    'author' => '',
+    'author_email' => '',
+    'author_company' => 'FTI',
+    'constraints' =>
+        array (
+            'depends' =>
+                array (
+                    'php' => '5.3.7-0.0.0',
+                    'typo3' => '6.2.0-6.2.99',
+                ),
+            'conflicts' =>
+                array (
+                ),
+            'suggests' =>
+                array (
+                ),
+        ),
+);
