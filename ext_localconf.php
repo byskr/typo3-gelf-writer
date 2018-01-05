a92bc4c5c41a2828ec265d341b1b4df9d5de09d4
Daniel Weise <dw@byskr.com>
2018-01-05 14:57:14 +0100

    Namespace changes

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,6 +2,6 @@
 
 $GLOBALS['TYPO3_CONF_VARS']['LOG']['writerConfiguration'] = [
     \TYPO3\CMS\Core\Log\LogLevel::DEBUG => [
-        'Byskr\\Typo3GelfWriter\\Writer\\GELFWriter' => []
+        'Byskr\\Typo3GELFWriter\\Writer\\GELFWriter' => []
     ]
 ];
